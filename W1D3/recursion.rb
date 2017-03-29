db558bd40524b0613d9e3df87bb585fa4f4323cb
Trevor O'Connor <trevoroc22@gmail.com>
2017-03-28 22:47:57 -0700

    Completes add_numbers

--- W1D3/recursion.rb
+++ W1D3/recursion.rb
@@ -16,4 +16,6 @@ end
 #
 # Returns the sum of all the elements of nums_array.
 def add_numbers(nums_array)
+  return nums_array.first if nums_array.length <= 1
+  nums_array.first + add_numbers(nums_array[1..-1])
 end
