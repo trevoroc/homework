f78568d23be852eacd4f7a06448670b3372de3b0
Trevor O'Connor <trevoroc22@gmail.com>
2017-03-30 21:47:55 -0700

    Write skeleton for the Stack class

---
+++ W1D5/stack.rb
@@ -0,0 +1,29 @@
+# Public: Class implementing the Stack data structure.
+class Stack
+
+  # Public: Initializes a new Stack.
+  def initialize
+
+  end
+
+  # Public: Pushes a new element onto the Stack.
+  #
+  # el - The Object to push onto the Stack.
+  #
+  # Returns nothing.
+  def add(el)
+
+  end
+
+  # Public: Pops an element from the Stack.
+  #
+  # Returns the element removed.
+  def remove
+  end
+
+  # Public: Creates a copy of this Stack.
+  #
+  # Returns a copy of the Stack.
+  def show
+  end
+end
