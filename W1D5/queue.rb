c37b075381994e28fe443a9a3ddf36eee5bfad91
Trevor O'Connor <trevoroc22@gmail.com>
2017-03-30 22:00:17 -0700

    Write skeleton for the Queue class

---
+++ W1D5/queue.rb
@@ -0,0 +1,27 @@
+# Public: Class representing the Queue data structure.
+class Queue
+
+  # Public: Initializes a new Queue.
+  def initialize
+  end
+
+  # Public: Enqueues a new element to the Queue.
+  #
+  # el - The Object to enqueue.
+  #
+  # Returns nothing.
+  def enqueue(el)
+  end
+
+  # Public: Dequeues the first element from the Queue.
+  #
+  # Returns the dequeued. element.
+  def dequeue
+  end
+
+  # Public: Creates a copy of this Queue.
+  #
+  # Returns the copy of this Queue.
+  def show
+  end
+end
