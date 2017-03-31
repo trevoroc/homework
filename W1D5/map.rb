fba27bd8eb01d69915bf3236908944226c6b57e3
Trevor O'Connor <trevoroc22@gmail.com>
2017-03-30 22:19:34 -0700

    Write skeleton for Map class

---
+++ W1D5/map.rb
@@ -0,0 +1,41 @@
+# Public: Class representing the Map data structure. This implementation will
+# only use arrays.
+class Map
+
+  # Public: Initializes a new Map.
+  def initialize
+  end
+
+  # Public: Adds a new key-value pair to this Map, or updates the value of an
+  # existing key.
+  #
+  # key - The key to add/update.
+  # value - The new value of key in this Map.
+  #
+  # Returns nothing.
+  def assign(key, value)
+  end
+
+  # Public: Retrieves the value of a specific key.
+  #
+  # key - The key whose value we wish to return.
+  #
+  # Returns the value associated with key in this Map, or nil if the key does
+  # not exist.
+  def lookup(key)
+  end
+
+  # Public: Removes a key-value pair from this Map.
+  #
+  # key - The key of the pair to be removed.
+  #
+  # Returns nothing.
+  def remove(key)
+  end
+
+  # Public: Creates a copy of this Map.
+  #
+  # Returns the copy of this Map.
+  def show
+  end
+end
