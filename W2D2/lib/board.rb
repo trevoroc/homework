afac5f3d8d3a926ffb80d5bcb76a4915d3aecfaa
Trevor O'Connor <trevoroc22@gmail.com>
2017-04-04 01:06:57 -0700

    Finish make_move, one_side_empty?, winner, fix initialize

--- W2D2/lib/board.rb
+++ W2D2/lib/board.rb
@@ -1,8 +1,10 @@
+require "byebug"
+
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    @cups = Array.new(14) { Array.new}
+    @cups = Array.new(14) { Array.new }
     @names = [name1, name2]
     place_stones
   end
@@ -14,16 +16,25 @@ class Board
   end
 
   def valid_move?(start_pos)
-    raise "Invalid starting cup" unless start_pos.between?(1, 14)
+    raise "Invalid starting cup" unless [1..6].concat([7..12]).include?(start_pos)
   end
 
   def make_move(start_pos, current_player_name)
-    stones, cups[start_pos] = cups[start_pos], []
-    current_pos = start_pos == 12 ? 0 : start_pos + 1
+    # debugger
+    start_pos -= 1 if start_pos.between?(1, 6)
+    skip = current_player_name == @names.first ? 13 : 6
+
+    stones, cups[start_pos] = cups[start_pos].dup, []
+    current_pos = start_pos + 1 == skip ? (start_pos + 2) % 14 : start_pos + 1
 
     until stones.empty?
+      if current_pos == skip
+        current_pos = (current_pos + 1) % 14
+        next
+      end
+
       cups[current_pos] << stones.pop
-      current_pos = current_pos == 12 ? 0 : current_pos + 1
+      current_pos = (current_pos + 1) % 14 unless stones.empty?
     end
 
     render
@@ -33,7 +44,14 @@ class Board
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
-    
+    if ending_cup_idx != 6 && ending_cup_idx != 13 &&
+       cups[ending_cup_idx].length == 1
+      :switch
+    elsif ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -45,8 +63,12 @@ class Board
   end
 
   def one_side_empty?
+    cups[0..5].all? { |cup| cup.empty? } || cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
+    return :draw if cups[6].length == cups[13].length
+    cups[6].length > cups[13].length ? @names.first : @names.last
   end
+
 end
