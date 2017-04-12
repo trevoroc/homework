bf6781d351279d53299fbe0e95742d94ed88ba92
Trevor O'Connor <trevoroc22@gmail.com>
2017-04-11 23:05:39 -0700

    Implement associations for House and Person

--- W3D3/Houses/app/models/person.rb
+++ W3D3/Houses/app/models/person.rb
@@ -1,4 +1,18 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id       :integer          not null, primary key
+#  name     :string           not null
+#  house_id :integer          not null
+#
+
 class Person < ActiveRecord::Base
   validates :name, presence: true
   validates :house_id, presence: true
+
+  belongs_to :house,
+             primary_key: :id,
+             foreign_key: :house_id,
+             class_name: :House
 end
