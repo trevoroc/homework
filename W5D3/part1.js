3ff3c515510139954611e26cd9d25ccf645563bf
Trevor O'Connor <trevoroc22@gmail.com>
2017-04-25 22:31:12 -0700

    Part 1: complete isSubstring

--- W5D3/part1.js
+++ W5D3/part1.js
@@ -3,3 +3,26 @@ function madLib(verb, adjective, noun) {
 }
 
 console.log(madLib("manipulate", "loving", "necklace"));
+
+function isSubstring(searchString, subString) {
+  let searchLength = searchString.length;
+  let subLength = subString.length;
+
+  if (searchLength < subLength) {
+    return false;
+  }
+
+  let substr;
+  for (let i = 0; i < searchLength - subLength; i++) {
+    substr = searchString.slice(i, i + subLength);
+
+    if (substr === subString) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+console.log(isSubstring("time to program", "time"));
+console.log(isSubstring("Jump for joy", "joys"));
