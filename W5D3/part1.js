1c6d0397f6462d06ca59510344f1e66c28e7eb9a
Trevor O'Connor <trevoroc22@gmail.com>
2017-04-25 22:43:26 -0700

    Part 1: complete fizzBuzz

--- W5D3/part1.js
+++ W5D3/part1.js
@@ -26,3 +26,26 @@ function isSubstring(searchString, subString) {
 
 console.log(isSubstring("time to program", "time"));
 console.log(isSubstring("Jump for joy", "joys"));
+
+function fizzBuzz(array) {
+  let divByThreeAndFive = [];
+
+  function divByThree(num) {
+    return num % 3 === 0;
+  }
+
+  function divByFive(num) {
+    return num % 5 === 0;
+  }
+
+  array.forEach(function (num) {
+    if ((divByThree(num) || divByFive(num)) &&
+       !(divByThree(num) && divByFive(num))) {
+      divByThreeAndFive.push(num);
+    }
+  });
+
+  return divByThreeAndFive;
+}
+
+console.log(fizzBuzz([1, 2, 3, 4, 5]));
