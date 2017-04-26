46a78b0df1ece5f224a9d82530059fea337c2f39
Trevor O'Connor <trevoroc22@gmail.com>
2017-04-25 23:40:31 -0700

    Part 2: complete Elephants and dinerBreakfast

--- W5D3/part2.js
+++ W5D3/part2.js
@@ -8,3 +8,64 @@ function printEach(array) {
 }
 
 titleize(["Mary", "Brian", "Leo"], printEach);
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+  this.height += 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function () {
+  let randomIndex = Math.floor(Math.random() * this.tricks.length);
+  console.log(`${this.name} is ${this.tricks[randomIndex]}!`);
+};
+
+const darrell = new Elephant("Darrell", 12, ["eating", "sleeping"]);
+darrell.trumpet();
+darrell.grow();
+console.log(darrell.height);
+darrell.play();
+darrell.addTrick("playing");
+darrell.play();
+darrell.play();
+darrell.play();
+
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+function paradeHelper(elephant) {
+  console.log(`${elephant.name} struts by.`);
+}
+
+herd.forEach(paradeHelper);
+
+function dinerBreakfast() {
+  let order = ["cheesy scrambled eggs"];
+  console.log(`I'd like ${order.join(' and ')} please.`);
+
+  return function(foodItem) {
+    order.push(foodItem);
+    console.log(`I'd like ${order.join(' and ')} please.`);
+  };
+}
+
+let bfastOrder = dinerBreakfast();
+bfastOrder("chocolate chips");
+bfastOrder("hash browns");
