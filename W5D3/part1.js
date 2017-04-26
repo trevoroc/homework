ac57087c114c02aa04b71bfa27a48583f5dedeca
Trevor O'Connor <trevoroc22@gmail.com>
2017-04-25 22:53:46 -0700

    Part 1: complete isPrime, sumOfNPrimes

--- W5D3/part1.js
+++ W5D3/part1.js
@@ -49,3 +49,39 @@ function fizzBuzz(array) {
 }
 
 console.log(fizzBuzz([1, 2, 3, 4, 5]));
+
+function isPrime(num) {
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isPrime(2));
+console.log(isPrime(4));
+console.log(isPrime(35));
+console.log(isPrime(37));
+
+function sumOfNPrimes(n) {
+  let sum = 0;
+  let count = 0;
+  let i = 2;
+
+  while (count < n) {
+    if (isPrime(i)) {
+      sum += i;
+      count++;
+    }
+
+    i++;
+  }
+
+  return sum;
+}
+
+console.log(sumOfNPrimes(0));
+console.log(sumOfNPrimes(1));
+console.log(sumOfNPrimes(4));
