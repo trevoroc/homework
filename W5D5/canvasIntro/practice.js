c27f394f7ce5fd70c2b4da77befa9b5ec4698a0b
Trevor O'Connor <trevoroc22@gmail.com>
2017-04-28 01:30:45 -0700

    Finish canvasIntro

--- W5D5/canvasIntro/practice.js
+++ W5D5/canvasIntro/practice.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById('myCanvas');
+  canvas.height = 500;
+  canvas.width = 500;
 
+  const ctx = canvas.getContext('2d');
+  // ctx.fillStyle = 'green';
+  // ctx.fillRect(100, 100, 400, 200);
+
+  ctx.beginPath();
+  ctx.strokeStyle = 'rgb(130, 40, 65)';
+  ctx.arc(250, 250, 75, 0, 2 * Math.PI);
+  ctx.lineWidth = 3;
+  ctx.stroke();
+
+  ctx.fillStyle = 'rbg(78, 82, 120)';
+  ctx.fill();
 });
