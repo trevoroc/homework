698a1c55c6d4b1e65636e1d2045d6023b8a81755
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-02 22:18:40 -0700

    Finish AJAX request

--- W6D3/js/index.js
+++ W6D3/js/index.js
@@ -1,5 +1,11 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  type: "GET",
+  success: weatherData => console.log(weatherData)
+});
 
 // Add another console log here, outside your AJAX request
+console.log("Asynchronous shenanigans!");
