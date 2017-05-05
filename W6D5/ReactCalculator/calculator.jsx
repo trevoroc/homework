b74718725503dbb57d708fb8a9f65f470f6dea5a
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-04 22:40:10 -0700

    Render result

--- W6D5/ReactCalculator/calculator.jsx
+++ W6D5/ReactCalculator/calculator.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    // TODO
+    this.state = {
+      result: 0,
+      num1: "",
+      num2: ""
+    };
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <section>
+        <h1>{this.state.result}</h1>
+      </section>
     );
   }
 }
