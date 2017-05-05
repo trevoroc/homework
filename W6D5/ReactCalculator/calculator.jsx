f04f9dbd73fa9a65513acdc9bcd0b322ac0b2a25
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-04 23:01:01 -0700

    Render input boxes and update state on change

--- W6D5/ReactCalculator/calculator.jsx
+++ W6D5/ReactCalculator/calculator.jsx
@@ -8,11 +8,30 @@ class Calculator extends React.Component {
       num1: "",
       num2: ""
     };
+
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+  }
+
+  setNum1(e) {
+    const newNum1 = document.getElementById("num1");
+    this.setState({ num1: newNum1.value });
+  }
+
+  setNum2(e) {
+    const newNum2 = document.getElementById("num2");
+    this.setState({ num2: newNum2.value });
   }
 
   render() {
+    console.log(this.state);
     return (
       <section>
+        <input onChange={this.setNum1} id="num1"
+         type="text" value={this.state.num1}/>
+        <input onChange={this.setNum2} id="num2"
+         type="text" value={this.state.num2}/>
+        <br/>
         <h1>{this.state.result}</h1>
       </section>
     );
