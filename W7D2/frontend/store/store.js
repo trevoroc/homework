67406fed4f4926c6b53e223ec472010e82664c7c
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-08 21:21:26 -0700

    Use library version of applyMiddleware, revert todo_redux

--- W7D2/frontend/store/store.js
+++ W7D2/frontend/store/store.js
@@ -1,12 +1,26 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 
+const addLoggingToDispatch = store => next => action => {
+  console.log(`Old state: ${store.getState()}`);
+  console.log(`Action: ${action}`);
+
+  let result = next(action);
+
+  console.log(`New state: ${store.getState()}`);
+
+  return result;
+};
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(RootReducer, preloadedState);
+  const store = createStore(RootReducer, preloadedState,
+                            applyMiddleware(addLoggingToDispatch));
+
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
+
   return store;
-}
+};
 
 export default configureStore;
--- W7D2/frontend/todo_redux.jsx
+++ W7D2/frontend/todo_redux.jsx
@@ -4,32 +4,10 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-const addLoggingToDispatch = store => next => action => {
-  console.log(`Old state: ${store.getState()}`);
-  console.log(`Action: ${action}`);
-
-  let result = next(action);
-
-  console.log(`New state: ${store.getState()}`);
-
-  return result;
-};
-
-const applyMiddleware = (store, middlewares) => {
-  let dispatch = store.dispatch;
-
-  middlewares.forEach((midware) => {
-    dispatch = midware(store)(dispatch);
-  });
-
-  return Object.assign({}, store, { dispatch });
-};
-
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   let store = configureStore(preloadedState);
-  store = applyMiddleware(store, [addLoggingToDispatch]);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
