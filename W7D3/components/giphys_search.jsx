54290b30b1038a2f9ffd62033b2ce6062e06fd31
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-10 12:41:49 -0700

    Stopped on search presentation

--- W7D3/components/giphys_search.jsx
+++ W7D3/components/giphys_search.jsx
@@ -1,3 +1,31 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ""
+    };
+  }
+
+  handleChange(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" value={ this.state.searchTerm }
+          onChange={ this.handleChange }></input>
+        <button onClick={ this.handleSubmit }>Search Giphys</button>
+      </form>
+    );
+  }
+}
