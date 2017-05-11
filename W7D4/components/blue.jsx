7d9bf365269ef1e4ea768610788eefa8297b3c5c
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-10 22:10:09 -0700

    Add color links

--- W7D4/components/blue.jsx
+++ W7D4/components/blue.jsx
@@ -9,11 +9,14 @@ class Blue extends React.Component {
       <div>
         <h2 className="blue"></h2>
         {/* Links here */}
+        <Link to="/blue" >Blue only</Link>
+        <Link to="/blue/indigo" >Add indigo</Link>
 
         {/* Routes here */}
+        <Route path="/blue/indigo" component={ Indigo } />
       </div>
     );
   }
-};
+}
 
 export default Blue;
